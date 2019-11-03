b200fea6ac9530bb113635bf5bb1801303ecc20e
Manuel Guerrini <man.guerrini@gmail.com>
2019-11-03 12:50:43 +0100

    creazione 16 numeri

--- myscript.js
+++ myscript.js
@@ -8,38 +8,29 @@
 // i 16 numeri vietati/mina, devono essere tutti diversi, non possono esserci doppioni;
 // l’utente non può inserire due volte lo stesso numero, ma sempre numeri diversi.
 
-// var mineArray = [];
+// genero i 16 numeri casuali da 1 a 100
 
-// mineArray deve essere lungo 16
-while(mineArray.length < 16) {
-   var mina = numRandom(1, 100);
-   var found = inArray(numero, mineArray);
-   if (found == false) {
-       mineArray.push(mina); // pusho solo se la mina non è già presente
-   }
-   console.log(found);
-   console.log('lunghezza array = ' + mineArray.length);
+var mineArray = []; // predispongo un array vuoto che ospiti le mine
+
+
+
+// per generare 16 mine devo ripetere la funzione 16 volte
+
+for(i=0; i < 16; i++) {
+  var mina = numRandom(1, 100); //invoco la funzione per generare una mina
+  console.log(mina);
 }
-console.log(mineArray);
 
-var maxNum = 84;
+// devo controllare che siano 16 mine diverse = potrei dover ciclare più di 16 volte
 
-var utenteArray = [];
+// se il numero non è all'interno dell'array lo metto dentro
 
-while (true) { // lo faccio fino a 84 tentativi, se non è presente il numero nel mineArray e se non ho inserito numeri identici
-  // while // se è nell'utenteArray
-  // prompt utente
-  var numero = parseInt(prompt('inserisci un numero'));
-  console.log('numero scelto = ' + numero);
-  if(found == true){
-    console.log('hai perso');
-  } else {
-     // controllo se siamo a maxNum
-    if (maxNum == 84 && found == false){
-      console.log('hai vinto');
-  }
-}
-}
+// chiedo all'utente un numero
+
+// se è uguale a una mina la partita termina
+// se è diversa si continua uguale
+
+// devo dare il punteggio finale = devo "conservare" i numeri diversi dalle mine e contarli (anche perchè il giocatore non può inserire numeri uguali)
 
 
 
@@ -50,15 +41,15 @@ function numRandom(min, max) {
   // il computer genera il suo numero
   return Math.floor((Math.random() * (max - min + 1)) + min);
 }
-
-function inArray(x, array) {
-  var found = false;
-  var i = 0;
-  while (found == false && i < array.length) {
-   if (array[i] == x)  {
-     found = true;
-   }
-   i++;
-  }
-  return found;
-}
+//
+// function inArray(x, array) {
+//   var found = false;
+//   var i = 0;
+//   while (found == false && i < array.length) {
+//    if (array[i] == x)  {
+//      found = true;
+//    }
+//    i++;
+//   }
+//   return found;
+// }
